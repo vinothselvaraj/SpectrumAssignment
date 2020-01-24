17d55a89132b9e1c9dfc8716ec61694150ba87a7
C02VM0S0HV2D <admin@Vinothmac.local>
2020-01-24 19:50:48 +0530

    Initial commit

---
+++ SpectrumAssignment/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SpectrumAssignment
+//
+//  Created by C02VM0S0HV2D on 24/01/20.
+//  Copyright © 2020 Vinoth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SpectrumAssignmentTests/SpectrumAssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpectrumAssignmentTests.swift
+//  SpectrumAssignmentTests
+//
+//  Created by C02VM0S0HV2D on 24/01/20.
+//  Copyright © 2020 Vinoth. All rights reserved.
+//
+
+import XCTest
+@testable import SpectrumAssignment
+
+class SpectrumAssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
